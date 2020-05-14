6667feeaa3260b21ec39fd8f98f9e3176538e32b
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-05-14 19:04:20 +0200

    wip

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,3 +1,11 @@
+import styled from 'styled-components'
 // exports a styled div
 // should bring in the .container styles inside styles.less
-
+const StyledContainer = styled.div`
+  border: 1px solid rgb(210, 210, 210);
+  box-shadow: 0px 1px 6px -2px rgb(128, 127, 127);
+  border-radius: 8px;
+  margin: 16px;
+  padding: 16px 8px 12px 16px;
+  background-color: white;
+`
