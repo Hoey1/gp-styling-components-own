5e7acd3aedfa910db2eeeb39d4f03c98ab0548ed
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-05-14 19:30:46 +0200

    wip

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -12,7 +12,7 @@ const StyledContainer = styled.div`
   border-radius: 8px;
   margin: 16px;
   padding: 16px 8px 12px 16px;
-  background-color: ${props => props.alert ? 'red' : 'white'};
+  background-color: ${props => props.alert ? 'red' : 'inherit'};
 
   &:hover {
     cursor: pointer;
