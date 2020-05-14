6403f5053fd9eb663397566367a93f4c304d642c
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-05-14 19:20:54 +0200

    wip

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,7 +2,12 @@ import styled from 'styled-components'
 // exports a styled div
 // should bring in the .container styles inside styles.less
 const StyledContainer = styled.div`
-  border: 1px solid rgb(210, 210, 210);
+  border: ${props => {
+    if (props.count) {
+      return props.count + 1
+    }
+    return '1'
+  }}px solid rgb(210, 210, 210);
   box-shadow: 0px 1px 6px -2px rgb(128, 127, 127);
   border-radius: 8px;
   margin: 16px;
